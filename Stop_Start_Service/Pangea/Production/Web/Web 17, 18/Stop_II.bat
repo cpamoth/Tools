f4751a800b087f996b6e08be2f4376dad1e6761d
Chris Amoth <camoth@geninfo.com>
2018-01-26 16:19:32 -0500

    removing more old scripts

--- Stop_Start_Service/Pangea/Production/Web/Web 17, 18/Stop_II.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\System32\inetsrv\appcmd stop apppool / RHI
-C:\Windows\System32\inetsrv\appcmd stop apppool / RHI2
--- Stop_Start_Service/Pangea/Production/Web/Web 23, 24/Stop_IIS.bat
+++
@@ -1,5 +0,0 @@
-C:\Windows\System32\inetsrv\appcmd stop apppool / NEXCOM
-C:\Windows\System32\inetsrv\appcmd stop apppool / WKU
-
-
-
